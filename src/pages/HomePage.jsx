b4a927b4b258079d59a1b10506008c608c22d801
Maksym Morenko <morenko.maxim@gmail.com>
2022-09-01 13:14:15 +0300

    Фильтрация по регионам

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,15 +7,15 @@ import { Card } from '../components/Card';
 import { Controls } from '../components/Controls';
 import { selectVisibleCountries, selectCountriesInfo } from '../store/countries/countries-selectors';
 import { loadCountries } from '../store/countries/countries-actions';
-import {selectSearch} from '../store/controls/controls-selectors'
+import {selectControls} from '../store/controls/controls-selectors'
 
 export const HomePage = () => {
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
-  const search = useSelector(selectSearch);
-  //передаем стейт (страны) и поиск в селектор, где будем фильтровать страны по поисковой строке
-  const countries = useSelector(state => selectVisibleCountries(state, {search})); 
+  const {search, region} = useSelector(selectControls);
+  //передаем стейт (страны) и поиск в селектор, где будем фильтровать страны по поисковой строке и регионам
+  const countries = useSelector(state => selectVisibleCountries(state, {search, region})); 
   const {status, error, qty} = useSelector(selectCountriesInfo);
 
   useEffect(() => {
--- src/store/countries/countries-selectors.js
+++ src/store/countries/countries-selectors.js
@@ -5,12 +5,12 @@ export const selectCountriesInfo = (state) => ({
 })
 
 export const selectAllCountries = (state) => state.countries.list;
-export const selectVisibleCountries = (state, {search = ''}) => {
+export const selectVisibleCountries = (state, {search = '', region = ''}) => {
   // кроме стейта на входе ждем информацюч о том что в поиске
   return state.countries.list.filter(
     country => (
-      // отфильтруем страны, оставим только ту страну которая содержит строку поисака
-      country.name.toLowerCase().includes(search.toLowerCase())
+      // отфильтруем страны, оставим только ту страну которая содержит строку поисака и выбраный регион
+      country.name.toLowerCase().includes(search.toLowerCase()) && country.region.includes(region)
     )
   )
 }
